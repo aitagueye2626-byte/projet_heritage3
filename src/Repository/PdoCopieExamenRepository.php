133738f0a55c0b8aae2ecf6ae32ba0521637728c
aitagueye-byyte <TON_EMAIL_GITHUB>
2026-09-03 11:44:12 +0000

    fix: correction de l'enregistrer d'une copie avec PDO

--- src/Repository/PdoCopieExamenRepository.php
+++ src/Repository/PdoCopieExamenRepository.php
@@ -2,15 +2,78 @@
 
 namespace App\Model;
 
-use PDO;
-use DateTime;
 use App\Entity\CopieExamen;
-use App\Service\CalculNoteAvecRetardService;
+use PDO;
 
-class PdoCopieExamenRepository implements CopieExamenRepositoryInterface
+
+final class PdoCopieExamenRepository extends Query implements CopieExamenRepositoryInterface
 {
-    public function __construct(
-        private PDO $pdo
-    ) {
+    private static ?PdoCopieExamenRepository $instance = null;
+
+    public static function getInstance(PDO $pdo): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self($pdo);
+        }
+        return self::$instance;
+    }
+    private function __construct(PDO $pdo)
+    {
+        parent::__construct($pdo);
+    }
+
+    public function save(CopieExamen $copieExamen): CopieExamen
+    {
+        $sql = "INSERT INTO copies_examen (note_brute, note_finale, penalite_appliquee, date_depot, date_limite)
+                VALUES (:note_brute, :note_finale, :penalite_appliquee, :date_depot, :date_limite)";
+
+        $id = $this->executeUpdate($sql, [
+            'note_brute' => $copieExamen->getNoteBrute(),
+            'note_finale' => $copieExamen->getNoteFinale(),
+            'penalite_appliquee' => $copieExamen->isPenaliteAppliquee() ? 1 : 0,
+            'date_depot' => $copieExamen->getDateDepot()->format('Y-m-d H:i:s'),
+            'date_limite' => $copieExamen->getDateLimite()->format('Y-m-d H:i:s'),
+        ]);
+
+        $copieExamen->setId((int) $id);
+
+        return $copieExamen;
+    }
+
+    public function findAll(): array
+    {
+        $lignes = $this->getAllData('copies_examen');
+
+        return array_map(
+            fn ($ligne) => $this->toEntity($ligne),
+            $lignes
+        );
+    }
+
+    public function findById(int $id): ?CopieExamen
+    {
+        $sql = "SELECT * FROM copies_examen WHERE id = :id";
+
+        $ligne = $this->executeQuery($sql, ['id' => $id]);
+
+        if (!$ligne) {
+            return null;
+        }
+
+        return $this->toEntity($ligne);
+    }
+
+    private function toEntity(\stdClass $ligne): CopieExamen
+    {
+        $copieExamen = new CopieExamen(
+            new \DateTimeImmutable($ligne->date_depot),
+            (float) $ligne->note_brute,
+            (bool) $ligne->penalite_appliquee,
+            new \DateTimeImmutable($ligne->date_limite),
+            (int) $ligne->id
+        );
+        $copieExamen->setNoteFinale((float) $ligne->note_finale);
+
+        return $copieExamen;
     }
 }

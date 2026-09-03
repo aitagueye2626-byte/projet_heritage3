1345144d0542b443cf46eaf6a74aad3b0569dba7
aitagueye-byyte <TON_EMAIL_GITHUB>
2026-09-03 11:44:53 +0000

    fix: rechercher les copies enregistrées

--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -2,23 +2,22 @@
 
 namespace App\Model;
 
-
 abstract class Repository
 {
-
     protected \PDO $connection;
 
     public function __construct(\PDO $connection)
     {
         $this->connection = $connection;
     }
-    private function query(string $sql, bool $single = true): mixed
+
+    protected function query(string $sql, bool $single = true): mixed
     {
         $query = $this->connection->query($sql);
 
         return $single
-            ? $query->fetch(PDO::FETCH_OBJ)
-            : $query->fetchAll(PDO::FETCH_OBJ);
+            ? $query->fetch(\PDO::FETCH_OBJ)
+            : $query->fetchAll(\PDO::FETCH_OBJ);
     }
 
     private function prepare(string $sql, array $datas): \PDOStatement
@@ -29,28 +28,28 @@ abstract class Repository
         return $prepare;
     }
 
-    private function executeQuery(string $sql, array $datas, bool $single = true): mixed
+    protected function executeQuery(string $sql, array $datas, bool $single = true): mixed
     {
         $statement = $this->prepare($sql, $datas);
 
         return $single
-            ? $statement->fetch(PDO::FETCH_OBJ)
-            : $statement->fetchAll(PDO::FETCH_OBJ);
+            ? $statement->fetch(\PDO::FETCH_OBJ)
+            : $statement->fetchAll(\PDO::FETCH_OBJ);
     }
 
-    private function executeUpdate(string $sql, array $datas): int
+    protected function executeUpdate(string $sql, array $datas): int
     {
         return $this->prepare($sql, $datas)->rowCount();
     }
 
-    private function insertAndGetId(string $sql, array $datas): int
+    protected function insertAndGetId(string $sql, array $datas): int
     {
-        $result = $this->prepare($sql, $datas)->fetch(PDO::FETCH_OBJ);
+        $result = $this->prepare($sql, $datas)->fetch(\PDO::FETCH_OBJ);
 
         return (int) $result->id;
     }
 
-    private function getAllData(string $tableName): array
+    protected function getAllData(string $tableName): array
     {
         return $this->query("SELECT * FROM $tableName", false);
     }
